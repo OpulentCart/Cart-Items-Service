f1dba3c0687fd69ac7082d13f522a403caeec4d7
Vedang <vedang.wappnet@gmail.com>
2025-02-25 17:51:50 +0530

    cart table is created

---
+++ models/cart.js
@@ -0,0 +1,30 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/dbConfig');
+
+const Cart = sequelize.define('Cart', {
+    cart_id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'auth_app_customuser',
+            key: 'id'
+        }
+    }
+}, {
+    tableName: 'cart',
+    timestamps: true
+});
+
+sequelize.sync({ alter: true}
+    .then(() => {
+        console.log("Category table is created")
+    })
+    .catch(err => console.error("❌ Error creating category table:", err))
+);
+
+module.exports = Cart;
