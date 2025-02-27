bf8bf5feb78230ff35bad2704ba81c42cca40cd9
Vedang <vedang.wappnet@gmail.com>
2025-02-27 11:31:21 +0530

    cart-service is created

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const { connectDB } = require("./config/dbConfig");
+require("dotenv").config();
+
+const cartRoutes = require('./routes/cartRoutes');
+const cartItemRoutes = require('./routes/cartItemRoutes');
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+// connect to the database
+connectDB();
+
+app.use('/carts', cartRoutes);
+app.use('/cart-items', cartItemRoutes);
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Cart-Items-Service is running on port ${process.env.PORT}`);
+});
